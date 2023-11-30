5cc35175e365309e3a1cb7d5ffee0acdab434a39
wintphyusinmaung <phyumaung423@gmail.com>
2023-11-30 14:02:56 +0630

    init: first commit

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Resources\PostCollection;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/posts', function () {
+    return new PostCollection(Post::all());
+});
